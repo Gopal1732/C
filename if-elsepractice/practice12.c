c3f6e2ab702322eaa52991cffb2f0dca299c5c1f
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-23 21:33:50 +0530

    total marks,percentage

--- if-elsepractice/practice12.c
+++ if-elsepractice/practice12.c
@@ -1,16 +1,43 @@
+//Write a C program to read roll no, name and marks of three subjects and calculate the total, percentage and division. 
 #include<stdio.h>
 
 int main(){
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    int roll,physics,chemistry,computer,total;
+    char name;
+    float percentage;
+    printf("roll number of student:");
+    scanf("%d",&roll);
+    printf("enter name of student:");
+    scanf("%s",&name);
+
+    printf("Enter marks in physics:");
+    scanf("%d",&physics);
+
+    printf("Enter marks in chemistry:");
+    scanf("%d",&chemistry);
+
+    printf("Enter marks in computer:");
+    scanf("%d",&computer);
+
+
+    total=physics+chemistry+computer;
+    printf("total marks:%d\n",total);
+
+    percentage=total*0.3;
+    printf("total percentage:%f\n",percentage);
+
+    if(percentage>90){
+        printf("first divison");
+    }
+    else if(percentage>=90 || percentage<80){
+        printf("second divison");
+    }
+    else if(percentage>=80 || percentage<70){
+        printf("third divison");
+    }
+    else{
+        printf("pass");
+    }
+
     return 0;
 }
