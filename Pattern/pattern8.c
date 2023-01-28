5ada351efcd16eda6102da658645551c6809c857
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-28 22:21:34 +0530

    right angled no.triangle

--- Pattern/pattern8.c
+++ Pattern/pattern8.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    int n,i,j;
+    int i,j,n;
+    printf("enter number:");
     scanf("%d",&n);
     for(i=1;i<=n;i++){
-        for(j=n;j>=1;j--)
-        {
-            printf("%d",j);
+        for(j=1;j<=i;j++){
+            printf("%d",i);
+
         }
         printf("\n");
     }
+    return 0;
 }
