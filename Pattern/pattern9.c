04fb46ebb9b6a65bc3abd2d806ae25e65d56d14e
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-28 22:38:59 +0530

    pyramid number

--- Pattern/pattern9.c
+++ Pattern/pattern9.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n,k=1;
+int main()
+{
+    int n, k = 1;
     printf("enter number:");
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j=j+1){
-            printf("%d",k);
-            k=k+1;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j = j + 1)
+        {
+            printf("%d", k);
+            k = k + 1;
         }
         printf("\n");
     }
