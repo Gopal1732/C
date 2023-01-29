ac54737bf6f57240aec6bacd67f5cbeabb51f4c5
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-29 22:52:42 +0530

    Right angled Triangle

--- Pattern/pattern1.c
+++ Pattern/pattern1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main(){
-    int i,j,n;
+    int i,j,m;
     printf("enter number:");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
+    scanf("%d",&m);
+    for(i=1;i<=m;i++){
         for(j=1;j<=i;j++){
             printf("*");
 
