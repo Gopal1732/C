498fafa0251335d91084856bd3731b69c4c142dc
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-30 23:48:05 +0530

    pattern32

---
+++ Pattern/pattern32.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    for(int i=1;i<=5;i++){
+        for(int s=5-i;s>=1;s--){
+            printf(" ");
+
+        }
+        for(j=1;j<=i;j++){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
