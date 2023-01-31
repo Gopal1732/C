c65d51941ae5c88f7ae191e46b3ba1f6ae856965
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-31 22:30:21 +0530

    Do while pattern

--- partternDOwhile.c
+++ partternDOwhile.c
@@ -4,18 +4,20 @@ int main(){
    int n;
    scanf("%d",&n);
 
-//    //pattern1
-//    int i=1;
-//    while(i<=n){
-//     int j=1;
-//     while(j<=n){
-//         printf("*");
-//         j++;
-//     }printf("\n");
-//     i++;
-//    }
+//pattern1
+   int i=1;
+   while(i<=n){
+    int j=1;
+    while(j<=n){
+        printf("*");
+        j++;
+    }printf("\n");
+    i++;
+   }
 
-      //pattern2
+
+
+//pattern2
    int i=1;
    while(i<=n){
     int j=1;
@@ -25,6 +27,98 @@ int main(){
     }printf("\n");
     i++;
    }
+
+
+
+
+// pattern3
+   int k=1;
+   int i=1;
+   while(i<=n){
+    int j=1;
+    while(j<=n){
+        printf("%d",k);
+        k=k+1;
+        j++;
+    }printf("\n");
+    i++;
+   }
+
+
+
+
+//pattern4
+   int i=1;
+   while(i<=n){
+    int j=1;
+    while(j<=i){
+        printf("*");
+        j++;
+    }printf("\n");
+    i++;
+   }
+
+
+
+
+//pattern5
+   int i=1;
+   while(i<=n){
+    int j=1;
+    while(j<=i){
+        printf("%d",i);
+        j++;
+    }printf("\n");
+    i++;
+   }
+
+
+
+
+
+//pattern6
+   int i=1;
+   while(i<=n){
+    int j=1,k=i;;
+    while(j<=i){
+        printf("%d",k);
+        k=k+1;
+        j++;
+    }printf("\n");
+    i++;
+   }
+
+
+
+
+
+// // pattern7
+    int i=1;
+    while(i<=n){
+    int j=i;
+    while(j>=1){
+        printf("%d",j);
+        j--;
+    }printf("\n");
+    i++;
+   }
+
+
+
+
+
+//pattern8
+   int i=1;
+   while(i<=n){
+    int j=1;
+    while(j<=n){
+     printf("%c",i+64);
+        j++;
+    }printf("\n");
+    i++;
+   }
+
+ 
 
    return 0;
 
