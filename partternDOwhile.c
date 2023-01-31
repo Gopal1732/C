7dc601ec6fe925c7cebb9aed59bdd603a183af86
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-31 22:00:55 +0530

    Do while pattern

---
+++ partternDOwhile.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+   int n;
+   scanf("%d",&n);
+
+   //pattern1
+   int i=1;
+   while(i<=n){
+    int j=1;
+    while(j<=n){
+        printf("*");
+        j++;
+    }printf("\n");
+    i++;
+   }
+
+   return 0;
+   
+}
