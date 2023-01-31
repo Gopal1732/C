4734b1eabbfbb9dac312d06e892e130b46b91451
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-31 11:06:45 +0530

    Reverse of number and palindrome

--- 31jan.c
+++ 31jan.c
@@ -4,13 +4,23 @@ int main(){
     int a,b,c=0;
     printf("enter any number:");
     scanf("%d",&a);
+    
+    int orgcopy=a;
 
     while(a>0){
         b=a%10;
         c=c*10+b;
         a=a/10;
     }
-    printf("REVERSE:-%d",c);
+    // printf("REVERSE:-%d",c);
+
+    if(orgcopy==c){
+        printf("palindrome");
+    }
+    else{
+        printf("not a palindrome");
+    }
+
 
     return 0;
 }
