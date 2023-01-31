36fa2ccf66ac97c4ecd4b7bab86b993e8f4bdac5
Gopal1732 <gopal.jana1704@gmail.com>
2023-01-31 22:03:27 +0530

    Do while pattern

--- partternDOwhile.c
+++ partternDOwhile.c
@@ -4,17 +4,28 @@ int main(){
    int n;
    scanf("%d",&n);
 
-   //pattern1
+//    //pattern1
+//    int i=1;
+//    while(i<=n){
+//     int j=1;
+//     while(j<=n){
+//         printf("*");
+//         j++;
+//     }printf("\n");
+//     i++;
+//    }
+
+      //pattern2
    int i=1;
    while(i<=n){
     int j=1;
     while(j<=n){
-        printf("*");
+        printf("%d",i);
         j++;
     }printf("\n");
     i++;
    }
 
    return 0;
-   
+
 }
