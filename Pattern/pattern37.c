77a0a4021e1a644c61ea9e3b944ea83fd1ef8f4e
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-01 23:57:40 +0530

    pattern using DO while

--- Pattern/pattern37.c
+++ Pattern/pattern37.c
@@ -3,24 +3,6 @@
 int main(){
     //  int n;
     //  scanf("%d",&n);
-    //  int i=1;
-    // while(i<=5){
-    //     int j=i ,k=1;
-    //     while(j<=5){
-    //         printf("%c",64+k);
-    //         k=k+1;
-    //         j++;
-    //     }
-    //     i++;
-    //     printf("\n");
-
-    // }
-
-    
-
-
-    //  int n;
-    //  scanf("%d",&n);
      int i=1;
     while(i<=5){
         int k=i;
