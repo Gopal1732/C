62af1c5be84c67a2e7af481f94d61afa21899a2f
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-04 21:26:48 +0530

    for loop2

---
+++ loops/question2.c
@@ -0,0 +1,12 @@
+// Write a C program to find the sum of first 10 natural numbers. 
+#include<stdio.h>
+
+int main(){
+    int sum=0;
+    for(int i=1;i<=10;i++){
+        sum =sum+i;
+    }
+     printf("%d",sum);
+    return 0;
+
+}
