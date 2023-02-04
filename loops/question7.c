5ea5e2e06406f09693a2a273770cd8115cf50bf8
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-04 22:15:05 +0530

    for loop7

---
+++ loops/question7.c
@@ -0,0 +1,21 @@
+ //Write a program in C to display the multiplication table vertically from 1 to n. 
+
+#include<stdio.h>
+
+int main(){
+    int n,sum=0;
+    int m;
+
+    printf("table to be calculated:");
+    scanf("%d",&n);
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=n;j++){
+             m=i*j;
+        printf(" %d x %d :-%d",j,i,m);
+        }
+        printf("\n");
+       
+    }
+
+    return 0;
+}
