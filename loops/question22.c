56a7edad5fff23021c1028a47855be37e3f46f99
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-06 21:26:41 +0530

    for loop22

--- loops/question22.c
+++ loops/question22.c
@@ -1,5 +1,14 @@
 //
 #include<stdio.h>
 int main(){
+    int n,sq,sum=0;
+    printf("enter number of terms:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        sq=i*i;
+        printf("%d ",sq);
+        sum=sum+sq;
+    }
     
+    printf("\nSum of natural n number:%d",sum);
 }
