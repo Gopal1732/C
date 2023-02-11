dc5ed9c49767a8693b62fb1e89291a715c936b3d
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-11 23:56:13 +0530

    Arrays

---
+++ Arrays/ques9.c
@@ -0,0 +1,34 @@
+ 
+ #include<stdio.h>
+
+int main(){
+    int n,i,j;
+    int arr[100],tmp;
+    
+    printf("Input the number of element:");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+ 
+ for(i=0; i<n; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            if(arr[j] <arr[i])
+            {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    
+    printf("elements in a sorted ascending order");
+    for(i=0;i<n;i++){
+        printf(" %d ",arr[i]);
+    }
+
+    printf("\n \n");
+}
