caf5217e3784d6b3897df568559e465f506047a9
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-13 21:27:49 +0530

    Arrays

---
+++ Arrays/ques19.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(){
+    int i,j,n;
+    int arr[50][50];
+
+    printf("input size of matrix:");
+    scanf("%d",&n);
+  
+
+    printf("Enter element in matrix:");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&arr[i][j]);
+           
+        }
+    }
+
+    printf("Matrix:-\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf(" %d ",arr[i][j]);
+        }printf("\n");
+    }
+
+    printf("Setting zero in lower triangle:-\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(i<=j){
+              printf(" %d ",arr[i][j]);  
+            }
+           else{
+            printf(" %d ",0);
+           }
+        }printf("\n");
+    }
+
+
+
+}
