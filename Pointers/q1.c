82779d017887ec4ce6abb0f0e95f58f91b0cb5b8
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-14 22:04:05 +0530

    Pointers

---
+++ Pointers/q1.c
@@ -0,0 +1,16 @@
+// Create a pointer and assign it a value.
+#include<stdio.h>
+
+void main()
+{
+    int x = 99;
+    // declare a pointer
+    int *ptr;
+    // assign value to pointer
+    ptr = &x;
+
+    printf("Value at ptr is: %d \n", *ptr);
+
+    printf("Address pointed by ptr is: %p \n", ptr);
+
+}
