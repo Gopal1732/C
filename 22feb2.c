5fd2fcc365e5dbe1676b2f111dc86521d795beae
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-22 14:01:38 +0530

    Recursion

---
+++ 22feb2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int div=2;
+int prime(int n){
+    if(div<n && n%div==0){
+        return 0;
+    }
+    else if(div==n){
+        return 1;
+    }
+    else{
+        div++;
+        prime(n); //recursive
+    }
+}
+
+int main(){
+    int num,ans;
+    printf("Enter any number: ");
+    scanf("%d",&num);
+
+    ans=prime(num);
+    if(ans==1){
+        printf("Prime number");
+    }
+    else{
+        printf("Not prime number");
+    }
+
+}
