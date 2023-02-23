c5855a98d16f6566aa73f9ce73b4a51855a29e50
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-23 22:45:23 +0530

    Enum

--- 23feb2.c
+++ 23feb2.c
@@ -3,29 +3,29 @@
 enum days{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday};
 
 int main(){
-    enum days d;
-    d=Tuesday;
-    switch(d){
+    enum days D;
+    D=Tuesday;
+    switch(D){
         case Sunday:
-        printf("Today is Sunday %d",d);
+        printf("Today is Sunday %d",D);
         break;
          case Monday:
-        printf("Today is Monday %d",d);
+        printf("Today is Monday %d",D);
         break;
          case Tuesday:
-        printf("Today is Tuesday %d",d);
+        printf("Today is Tuesday %d",D);
          break;
          case Wednesday:
-        printf("Today is Wednesday %d",d);
+        printf("Today is Wednesday %d",D);
          break;
          case Thursday:
-        printf("Today is Thursday %d",d);
+        printf("Today is Thursday %d",D);
          break;
          case Friday:
-        printf("Today is Friday %d",d);
+        printf("Today is Friday %d",D);
          break;
          case Saturday:
-        printf("Today is Saturday %d",d);
+        printf("Today is Saturday %d",D);
 
     }
 }
