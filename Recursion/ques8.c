ec6ec19a53d70bd4b4139707f0628c547f2543b2
Gopal1732 <gopal.jana1704@gmail.com>
2023-02-23 23:32:42 +0530

    Recursion

---
+++ Recursion/ques8.c
@@ -0,0 +1,24 @@
+ 
+#include<stdio.h>
+
+int power(int a,int b){
+    if(b==1){
+        return a;
+    }
+    else{
+        return a*power(a ,b-1);
+    }
+}
+
+int main(){
+    int n,pwr;
+    printf("Enter number:");
+    scanf("%d",&n);
+
+    printf("Enter power:");
+    scanf("%d",&pwr);
+
+    printf("Power:%d ",power(n,pwr));
+
+
+}
